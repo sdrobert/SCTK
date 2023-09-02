412b918ba15b075e99589d225749628c73c01d98
Sean Robertson <sdrobert@cs.toronto.edu>
2023-09-01 20:22:23 -0400

    Some initial presets

--- src/rfilter1/include/calloc2.c
+++ src/rfilter1/include/calloc2.c
@@ -13,7 +13,7 @@
   if (db_level > 3) printf("%sdoing %s\n",(char *)pdb,(char *)proc);
   x = calloc(nobj,size);
   if (x == NULL) fatal_error(calling_proc,(Char *)"MEM ALLOC",-1);
-  if (memory_trace) printf("%s CALLOC %lx\n",pdb,x);
+  if (memory_trace) printf("%s CALLOC %p\n",pdb, (void *)x);
   return x;
  }
 /* end calloc2.c */
--- src/rfilter1/include/strdup2.c
+++ src/rfilter1/include/strdup2.c
@@ -15,7 +15,7 @@ if (db_level > 3) printf("%sdoing %s\n",pdb,proc);
  pd = (Char*)malloc((size_t)strlen((char *)ps)+1);
  if (pd == NULL) fatal_error(calling_proc,(Char *)"MEM ALLOC",-1);
   else   pd = (Char *)strcpy((char *)pd,(char *)ps);
- if (memory_trace) printf("%s MALLOC %lx\n",pdb,pd);
+ if (memory_trace) printf("%s MALLOC %p\n",pdb,(void *) pd);
   return pd;
  }
 /* end strdup2.c */
